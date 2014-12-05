8054aaac31d9b918636773e71f364f85c8a3bca5
chris <c.goldstein@goldstein-systemberatung.de>
2014-12-05 22:58:49 +0100

    5

--- Raytracer/rt/coordmappers/plane.cpp
+++ Raytracer/rt/coordmappers/plane.cpp
@@ -17,11 +17,7 @@ PlaneCoordMapper::PlaneCoordMapper(const Vector& e1, const Vector& e2): e1(e1), 
 }
 
 Point PlaneCoordMapper::getCoords(const Intersection& hit) const {
-	Vector normal = cross(e1, e2).normalize();
 
-	Float4 f4 = Float4(hit.localPoint.x * e1) + Float4(hit.localPoint.y * e2);
-	f4.w = 1;
-	f4.z = 0;
 
 	return Point(f4);
 }
