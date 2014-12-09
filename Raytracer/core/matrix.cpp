5252dbba28e3563bb17fe98a8423bb16001b5899
s9mibier <s9mibier@stud.uni-saarland.de>
2014-12-09 15:07:18 +0100

    Dumdidum

--- Raytracer/core/matrix.cpp
+++ Raytracer/core/matrix.cpp
@@ -108,7 +108,7 @@ Matrix Matrix::operator-(const Matrix& b) const {
 }
 
 Matrix Matrix::transpose() const {
-	return (Float4(rows[0][0], rows[1][0], rows[2][0], rows[3][0]), Float4(
+	return Matrix(Float4(rows[0][0], rows[1][0], rows[2][0], rows[3][0]), Float4(
 			rows[0][1], rows[1][1], rows[2][1], rows[3][1]), Float4(rows[0][2],
 			rows[1][2], rows[2][2], rows[3][2]), Float4(rows[0][3], rows[1][3],
 			rows[2][3], rows[3][3]));
