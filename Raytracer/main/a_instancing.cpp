c4c8a0699ef310e30d353e8ac1e4c83c7f36f20d
chris <c.goldstein@goldstein-systemberatung.de>
2014-12-11 14:48:38 +0100

    translate correct

--- Raytracer/main/a_instancing.cpp
+++ Raytracer/main/a_instancing.cpp
@@ -76,33 +76,33 @@ void a_instancing() {
 
     Instance* normal = new Instance(tree);
     scene->add(normal);
-
+/*
     float circleRadius = 18.0f;
-    for (int i = 0; i<3; ++i) {
+    for (int i = 0; i<11; ++i) {
         float angle = 2.0f*i*pi / 11.0f;
         Instance* itree = new Instance(tree);
-        itree->scale(Vector(1.0f,1.0f + sin(i*1.0f)*0.3f,1.0f));
+        //itree->scale(Vector(1.0f,1.0f + sin(i*1.0f)*0.3f,1.0f));
         itree->translate(Vector(sin(angle) * circleRadius, 0.0f, cos(angle) * circleRadius));
         scene->add(itree);
     }
-/*
+
     {
     Instance* broken = new Instance(tree);
-    broken->rotate(Vector(0.3f, 0.0f, 0.7f),-1.0f);
+    //broken->rotate(Vector(0.3f, 0.0f, 0.7f),-1.0f);
     broken->translate(Vector(5.0f, -0.5f, 0.0f));
     scene->add(broken);
     }
-
+*/
     {
     Instance* broken = new Instance(tree);
     broken->rotate(Vector(0.7f, 0.0f, -0.3f),-1.0f);
-    broken->translate(Vector(5.0f, -0.5f, 0.0f));
+    //broken->translate(Vector(5.0f, -0.5f, 0.0f));
     scene->add(broken);
     }
-
+/*
     {
     Instance* flat = new Instance(tree);
-    flat->scale(Vector(0.3f, 1.5f, 1.5f));
+    //flat->scale(Vector(0.3f, 1.5f, 1.5f));
     flat->translate(Vector(-11.0f, 0.0f, 0.0f));
     scene->add(flat);
     }
