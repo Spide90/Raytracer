8cd5a2e03e4cb09f5eb7bd212db58738478da986
s9mibier <s9mibier@stud.uni-saarland.de>
2014-12-12 12:10:35 +0100

    Komm mit! Lauf weg!

--- Raytracer/main/a_instancing.cpp
+++ Raytracer/main/a_instancing.cpp
@@ -76,8 +76,8 @@ void a_instancing() {
 
     Instance* normal = new Instance(tree);
     scene->add(normal);
-/*
-    float circleRadius = 18.0f;
+
+  float circleRadius = 18.0f;
     for (int i = 0; i<11; ++i) {
         float angle = 2.0f*i*pi / 11.0f;
         Instance* itree = new Instance(tree);
@@ -85,7 +85,7 @@ void a_instancing() {
         itree->translate(Vector(sin(angle) * circleRadius, 0.0f, cos(angle) * circleRadius));
         scene->add(itree);
     }
-*/
+
     {
     Instance* broken = new Instance(tree);
     broken->rotate(Vector(0.3f, 0.0f, 0.7f),-1.0f);
@@ -99,14 +99,14 @@ void a_instancing() {
     broken->translate(Vector(5.0f, -0.5f, 0.0f));
     scene->add(broken);
     }
-/*
+
     {
     Instance* flat = new Instance(tree);
     flat->scale(Vector(0.3f, 1.5f, 1.5f));
     flat->translate(Vector(-11.0f, 0.0f, 0.0f));
     scene->add(flat);
     }
-*/
+
     scene->add(new InfinitePlane(Point(0,0,0), Vector(0, 1, 0), nullptr, nullptr));
     scene->rebuildIndex();
 
