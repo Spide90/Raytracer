92cf5c092ccf19c06f5c0b6e32809eaccf43738e
s9mibier <s9mibier@stud.uni-saarland.de>
2015-01-08 16:51:31 +0100

    fuzzyfuzzy willnet...

--- Raytracer/rt/materials/fuzzymirror.cpp
+++ Raytracer/rt/materials/fuzzymirror.cpp
@@ -7,26 +7,79 @@
 
 #include <rt/materials/fuzzymirror.h>
 #include <rt/materials/material.h>
+#include <core/random.h>
+#include <core/matrix.h>
+#include <core/point.h>
+#include <core/float4.h>
+#include <math.h>
 
 namespace rt {
 
 FuzzyMirrorMaterial::FuzzyMirrorMaterial(float eta, float kappa,
-		float fuzzyangle) : eta(eta), kappa(kappa), fuzzyAngle(fuzzyangle) {
+		float fuzzyangle) :
+		eta(eta), kappa(kappa), fuzzyAngle(fuzzyangle) {
 }
 
-RGBColor FuzzyMirrorMaterial::getReflectance(const Point& texPoint, const Vector& normal,
-		const Vector& outDir, const Vector& inDir) const {
+RGBColor FuzzyMirrorMaterial::getReflectance(const Point& texPoint,
+		const Vector& normal, const Vector& outDir, const Vector& inDir) const {
+	float e2_k2 = (eta * eta) + (kappa * kappa);
+	float cosine = dot(inDir, normal);
+	float cosine_2 = cosine * cosine;
+	float r_par_2 = (e2_k2 * cosine_2 - 2 * eta * cosine + 1)
+			/ (e2_k2 * cosine_2 + 2 * eta * cosine + 1);
+	float r_per_2 = (e2_k2 - 2 * eta * cosine + cosine_2)
+			/ (e2_k2 + 2 * eta * cosine + cosine_2);
 
+	return 0.5
+			* RGBColor(r_par_2 + r_per_2, r_par_2 + r_per_2, r_par_2 + r_per_2);
 }
 
-RGBColor FuzzyMirrorMaterial::getEmission(const Point& texPoint, const Vector& normal,
-		const Vector& outDir) const {
-
-}
-
-Material::SampleReflectance FuzzyMirrorMaterial::getSampleReflectance(const Point& texPoint,
+RGBColor FuzzyMirrorMaterial::getEmission(const Point& texPoint,
 		const Vector& normal, const Vector& outDir) const {
+	return RGBColor(0, 0, 0);
+}
 
+Material::SampleReflectance FuzzyMirrorMaterial::getSampleReflectance(
+		const Point& texPoint, const Vector& normal,
+		const Vector& outDir) const {
+	Vector dir = (2 * dot(outDir, normal) * normal - outDir).normalize();
+
+	float r1 = random(fuzzyAngle);
+	float r2 = random(sqrt(fuzzyAngle));
+
+	Point center = texPoint + dir;
+
+	Vector span1 = cross(outDir, normal).normalize();
+	Vector span2 = cross(outDir, span1).normalize();
+
+	Point npoint = center + span1 * fuzzyAngle + span2 * sqrt(fuzzyAngle);
+	Vector ndir = npoint - texPoint;
+
+//	Vector axis = dir;
+//
+//	Matrix scaleMatrix(Float4(cosf(angle), 0, 0, 0), Float4(0, cosf(angle), 0, 0),
+//			Float4(0, 0, cosf(angle), 0), Float4(0, 0, 0, 1));
+//	Matrix rot1(Float4(0, -axis.z * sinf(angle), axis.y * sinf(angle), 0),
+//			Float4(axis.z * sinf(angle), 0, -axis.x * sinf(angle), 0),
+//			Float4(-axis.y * sinf(angle), axis.x * sinf(angle), 0, 0),
+//			Float4(0, 0, 0, 0));
+//	Matrix rot2(
+//			Float4(axis.x * axis.x * (1 - cosf(angle)),
+//					axis.x * axis.y * (1 - cosf(angle)),
+//					axis.x * axis.z * (1 - cosf(angle)), 0),
+//			Float4(axis.x * axis.y * (1 - cosf(angle)),
+//					axis.y * axis.y * (1 - cosf(angle)),
+//					axis.z * axis.y * (1 - cosf(angle)), 0),
+//			Float4(axis.x * axis.z * (1 - cosf(angle)),
+//					axis.y * axis.z * (1 - cosf(angle)),
+//					axis.z * axis.z * (1 - cosf(angle)), 0),
+//			Float4(0, 0, 0, 0));
+//	rot2 = rot2 * (1/(axis.length()*axis.length()));
+//	rot1 = rot1 * (1/axis.length());
+//	Matrix rot = scaleMatrix + (rot1 + rot2);
+
+	return Material::SampleReflectance(dir,
+			this->getReflectance(texPoint, normal, outDir, ndir));
 }
 
 }
--- Raytracer/rt/materials/glass.cpp
+++ Raytracer/rt/materials/glass.cpp
@@ -8,6 +8,7 @@
 #include <rt/materials/glass.h>
 #include <rt/materials/material.h>
 #include <math.h>
+#include <core/random.h>
 
 namespace rt {
 
@@ -17,15 +18,18 @@ GlassMaterial::GlassMaterial(float eta) :
 
 RGBColor GlassMaterial::getReflectance(const Point& texPoint,
 		const Vector& normal, const Vector& outDir, const Vector& inDir) const {
-	float angle = dot(inDir.normalize(), normal);
-	float ueta = ueta;
-	if(!(acosf(angle) > M_PI / 4.f)){
-		ueta = 1 / ueta;
-	}
+	float cosI = -dot(inDir, normal);
+	float sinI2 = eta * eta * (1 - cosI * cosI);
+	float cosT = sqrt(1 - sinI2);
 
-	float angle2 = ueta * angle;
+	float ueta = eta;
 
-	return RGBColor(angle2, angle2, angle2);
+	float r_Orth = (cosI - ueta * cosT) / (cosI + ueta * cosT);
+	float r_Par = (ueta * cosI - cosT) / (ueta * cosI + cosT);
+
+	float ret = (r_Orth * r_Orth + r_Par * r_Par) * 0.5;
+
+	return RGBColor(ret, ret, ret);
 }
 
 RGBColor GlassMaterial::getEmission(const Point& texPoint, const Vector& normal,
@@ -36,7 +40,27 @@ RGBColor GlassMaterial::getEmission(const Point& texPoint, const Vector& normal,
 Material::SampleReflectance GlassMaterial::getSampleReflectance(
 		const Point& texPoint, const Vector& normal,
 		const Vector& outDir) const {
-	//TODO implement me!
+	float blubb = random();
+
+	if (blubb >= 0.5) {
+		Vector dir = (2 * dot(outDir, normal) * normal - outDir).normalize();
+		return Material::SampleReflectance(dir,
+				this->getReflectance(texPoint, normal, outDir, dir));
+	} else {
+		float angle = acosf(dot(outDir, normal));
+		float ueta = eta;
+		if (angle > M_PI / 2) {
+			ueta = 1 / ueta;
+		}
+
+		float cos_phi_i = dot(outDir, normal);
+		float sin_phi_t_2 = ueta * ueta
+				* (1 - cos_phi_i * cos_phi_i);
+		Vector dir = ueta * outDir
+				+ (ueta * cos_phi_i - sqrt(1 - sin_phi_t_2)) * normal;
+		return Material::SampleReflectance(dir,
+				this->getReflectance(texPoint, normal, outDir, dir));
+	}
 }
 
 }
