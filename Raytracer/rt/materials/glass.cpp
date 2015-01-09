ba107ce641a5648d5a96d5e0f07a66bbab1513de
s9mibier <s9mibier@stud.uni-saarland.de>
2015-01-09 12:13:45 +0100

    glass

--- Raytracer/rt/materials/glass.cpp
+++ Raytracer/rt/materials/glass.cpp
@@ -20,9 +20,20 @@ RGBColor GlassMaterial::getReflectance(const Point& texPoint,
 		const Vector& normal, const Vector& outDir, const Vector& inDir) const {
 	float cosI = dot(inDir, normal);
 	float ueta = eta;
-	float sinI2 = ueta * ueta * (1 - cosI * cosI);
+	Vector unormal = normal;
 	float n1 = 1.f;
 	float n2 = 1.f / ueta;
+
+	if(cosI < 0){
+		LOG_DEBUG("test");
+		unormal = -unormal;
+		cosI = dot(outDir, unormal);
+		n1 = 1 / ueta;
+		n2 = 1.f;
+		ueta = 1 / eta;
+	}
+
+	float sinI2 = ueta * ueta * (1 - cosI * cosI);
 
 	if(sinI2 > 1.f){
 		return RGBColor(1.f, 1.f, 1.f);
@@ -72,11 +83,11 @@ Material::SampleReflectance GlassMaterial::getSampleReflectance(
 	}
 
 	float cosT = sqrtf(1 - sinI2);
-
-	float r_Orth = (n1 * cosI - n2 * cosT) / (n1 * cosI + n2 * cosT);
-	float r_Par = (n2 * cosI - n1 * cosT) / (n2 * cosI + n1 * cosT);
-
-	float ret = (r_Orth * r_Orth + r_Par * r_Par) * 0.5;
+//
+//	float r_Orth = (n1 * cosI - n2 * cosT) / (n1 * cosI + n2 * cosT);
+//	float r_Par = (n2 * cosI - n1 * cosT) / (n2 * cosI + n1 * cosT);
+//
+//	float ret = (r_Orth * r_Orth + r_Par * r_Par) * 0.5;
 
 	float blubb = random();
 
@@ -84,15 +95,17 @@ Material::SampleReflectance GlassMaterial::getSampleReflectance(
 		dir = (2 * dot(outDir, unormal) * unormal - outDir).normalize();
 //		return Material::SampleReflectance(dir,
 //				2.f * RGBColor(ret, ret, ret));
+		return Material::SampleReflectance(dir,
+						2.f * (this->getReflectance(texPoint, unormal, outDir, dir)));
 	} else {
 		dir = (-ueta * outDir
 				+ (ueta * cosI - cosT) * unormal).normalize();
-		ret = 1 - ret;
-//		return Material::SampleReflectance(dir,
-//				2.f * (this->getReflectance(texPoint, unormal, outDir, dir)));
+//		ret = 1 - ret;
+		return Material::SampleReflectance(dir,
+				2.f * (this->getReflectance(texPoint, unormal, outDir, dir)));
 	}
-	return Material::SampleReflectance(dir,
-			2.f * RGBColor(ret, ret, ret));
+//	return Material::SampleReflectance(dir,
+//			2.f * RGBColor(ret, ret, ret));
 }
 
 }
