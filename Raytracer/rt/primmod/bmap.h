33942a32d10ff7c9719e6e8c6c9c4a0afbfaf469
chris <c.goldstein@goldstein-systemberatung.de>
2015-01-14 18:11:14 +0100

    bump mapping started

--- Raytracer/rt/primmod/bmap.h
+++ Raytracer/rt/primmod/bmap.h
@@ -12,6 +12,12 @@ class Texture;
 
 class BumpMapper : public Primitive {
 public:
+
+	Triangle* triangleBase;
+	Texture* bumpTexture;
+	Point textureEdges[3];
+	float bumpScale;
+
     BumpMapper(Triangle* base, Texture* bumpmap, const Point& bv1, const Point& bv2, const Point& bv3, float vscale);
     virtual BBox getBounds() const;
     virtual Intersection intersect(const Ray& ray, float previousBestDistance=FLT_MAX) const;
