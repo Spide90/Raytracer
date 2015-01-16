113f62b4363b4821d59342b5ad115219dd70c883
s9mibier <s9mibier@stud.uni-saarland.de>
2015-01-16 11:47:45 +0100

    fixed

--- Raytracer/rt/primmod/bmap.cpp
+++ Raytracer/rt/primmod/bmap.cpp
@@ -18,8 +18,8 @@
 
 namespace rt {
 
-BumpMapper::BumpMapper(Triangle* base, Texture* bumpmap, const Point& bv1, const Point& bv2, const Point& bv3,
-		float vscale) :
+BumpMapper::BumpMapper(Triangle* base, Texture* bumpmap, const Point& bv1,
+		const Point& bv2, const Point& bv3, float vscale) :
 		triangleBase(base), bumpTexture(bumpmap), bumpScale(vscale) {
 	textureEdges[0] = bv1;
 	textureEdges[1] = bv2;
@@ -30,10 +30,13 @@ BBox BumpMapper::getBounds() const {
 	triangleBase->getBounds();
 }
 
-Intersection BumpMapper::intersect(const Ray& ray, float previousBestDistance) const {
-	Intersection intersection = triangleBase->intersect(ray, previousBestDistance);
+Intersection BumpMapper::intersect(const Ray& ray,
+		float previousBestDistance) const {
+	Intersection intersection = triangleBase->intersect(ray,
+			previousBestDistance);
 	if (intersection) {
-		Float4 bumpMapPoint = Float4(intersection.local().x * textureEdges[0]) + Float4(intersection.local().y * textureEdges[1])
+		Float4 bumpMapPoint = Float4(intersection.local().x * textureEdges[0])
+				+ Float4(intersection.local().y * textureEdges[1])
 				+ Float4(intersection.local().z * textureEdges[2]);
 		bumpMapPoint.w = 1;
 
@@ -44,12 +47,19 @@ Intersection BumpMapper::intersect(const Ray& ray, float previousBestDistance) c
 		//these should be the texture base vectors mapped to world space
 //		Vector worldX = (textureEdges[1] - textureEdges[0]).normalize() * colorX.r;
 //		Vector worldY = (textureEdges[2] - textureEdges[0]).normalize() * colorY.g;
-		Vector worldX = (triangleBase->edges[1] - triangleBase->edges[0]) * colorX.r;
-		Vector worldY = (triangleBase->edges[2] - triangleBase->edges[0]) * colorY.g;
+		Vector worldX = (triangleBase->edges[1] - triangleBase->edges[0]).normalize();
+		Vector worldY = (triangleBase->edges[2] - triangleBase->edges[0]).normalize();
+
+		Vector newNormal = cross(worldX, worldY)
+				- colorX.r * cross(intersection.normal(), worldY)
+				+ colorY.g * cross(intersection.normal(), worldX);
+//				+ colorX.r * colorY.g
+//						* cross(intersection.normal(), intersection.normal());
 
 		//pertub the normal with worldX, worldY and the color gradient
 //		intersection.normalVector = cross(worldX, worldY).normalize() * bumpScale;
-		intersection.normalVector = (intersection.normalVector + worldX + worldY).normalize();
+//		intersection.normalVector = (intersection.normalVector + worldX + worldY).normalize();
+		intersection.normalVector = newNormal.normalize();
 	}
 	return intersection;
 }
